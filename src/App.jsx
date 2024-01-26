45a283993e7365a24fc154c3f5a8831612fc8689
Sushant <sush1393@gmail.com>
2024-01-26 12:14:06 -0500

    passing props from a local json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import Lesson01 from "../jsx-basics/jsx-basics";
-import { ToDoListClass } from "../components-intro/TodoListClass";
-import { FunctionProps } from "../functionProps/FunctionProps";
-import { TodoListItem } from "../components-intro/TodoListItem";
-import { TodoListItemClass } from "../components-intro/TodoListItemClass";
-// import { TodoList } from "../lesson-02/TodoList";
-import "./styles.css";
+import { UserCard } from "../user-card/UserCard";
+import { UserCardClass } from "../user-card/UserCardClass";
+import "../user-card/user.css";
+import userData from "../user-card/user.json";
 
 function App() {
-  // return <TodoList />;
-  // return <ToDoListClass />;
   return (
     <>
-      {/* <FunctionProps name="Sushant" age={27} />
-      <FunctionProps name="Sush" age={25} isProgrammer />
-      <FunctionProps>
-        <span>Sushi </span>
-        <span>28</span>
-      </FunctionProps> */}
-      <TodoListItem isComplete>Item 1</TodoListItem>
-      <TodoListItemClass isComplete>Item 2</TodoListItemClass>
+      <UserCard {...userData}></UserCard>
+      <br />
+      <UserCardClass {...userData}></UserCardClass>
     </>
   );
 }
