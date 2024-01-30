275c1aa4d8ca75d07055ebdfd344296b8a083916
Sushant <sush1393@gmail.com>
2024-01-30 14:47:55 -0500

    react-router basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
 import React from "react";
-import Home from "../no-lib-routing/Home";
-import About from "../no-lib-routing/About";
-import Contact from "../no-lib-routing/Contact";
-
 // import "./styles.css";
 
 function App() {
-  let component;
-  const url = window.location.pathname;
-  switch (url) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/about":
-      component = <About />;
-      break;
-    case "/contact":
-      component = <Contact />;
-      break;
-
-    default:
-      break;
-  }
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      {component}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
