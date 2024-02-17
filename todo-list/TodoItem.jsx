e88a994fafe4541c17b17eea31cb2c9377de8b94
Sushant <sush1393@gmail.com>
2024-02-17 16:38:28 -0500

    add css to show strikethrough for completed tasks

--- todo-list/TodoItem.jsx
+++ todo-list/TodoItem.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import "./todoItem.css";
 
 const TodoItem = ({ id, name, completed, toggleTask, deleteTask }) => {
   return (
     <div>
       <input
+        data-list-item-checkbox
         type="checkbox"
         name="task"
         id="task"
         checked={completed}
         onChange={(e) => toggleTask(id, e.target.checked)}
       />
-      <span>{name}</span>
+      <span data-list-item-text>{name}</span>
       <button type="button" onClick={() => deleteTask(id)}>
         Delete
       </button>
